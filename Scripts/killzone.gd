eb5c6f6c305738433a5d77f8b65ed20dea923ff2
Lore <lorenyanski@gmail.com>
2025-02-15 11:28:46 +0100

    Miserable attempt at variable killtimer
    animation players my unbeloved

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,16 +1,30 @@
 extends Area2D
 
 @export var shader_material: ShaderMaterial  # Assign this in the editor
-@export var timer: float
+@export_range(2,10) var timer: float
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	animation_player.speed_scale = 3/timer
 	#var kill = Animation.new()
 	#kill.length = timer
 	#kill.loop_mode = 1
-	#kill.add_track(Animation.TYPE_ANIMATION,0)
-	
+	#
+	#kill.add_track(Animation.TYPE_VALUE,0)
+	#kill.track_set_path(0, "Sprite2D:modulate")
+	#
+	#kill.track_insert_key(0, 0, Color(255,0,0,0))
+	#kill.track_insert_key(0, timer-0.4, Color(255,0,0,0))
+	#kill.track_insert_key(0, timer-0.3, Color(255,0,0,169))
+	#
+	#kill.add_track(Animation.TYPE_METHOD,1)
+	#kill.track_set_path(1, ".")
+	#
+	#kill.track_insert_key(1,timer-0.3,kill_player)
+	#
+	#animation_player.get_animation_library("").add_animation("kill",kill)
+	#animation_player.play("kill")
 
 func _process(delta):
 	var rat = get_node_or_null("../../Mouse/Sprite2D")
@@ -30,5 +44,7 @@ func _process(delta):
 		shader_material.set_shader_parameter("mask_position", uv_mask_pos)
 		shader_material.set_shader_parameter("mask_size", uv_mask_size)  # Pass size
 
-func kill():
+func kill_player():
 	print('IM KILLING YOU >:((')
+	if has_overlapping_areas():
+		print('L')
