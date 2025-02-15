a12b8b6f70cf21e35fc067727aab843fb0336575
Lore <lorenyanski@gmail.com>
2025-02-15 03:25:31 +0100

    r o t a t e

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -3,6 +3,9 @@ extends Area2D
 var velocity: Vector2
 const FRICTION = 0.98
 
+@onready var sprite_2d = $Sprite2D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = Vector2(0,0)
@@ -11,6 +14,8 @@ func _ready():
 func _process(delta):
 	position += velocity*delta
 	velocity = (velocity + get_local_mouse_position()*delta*4)*FRICTION
+	
+	sprite_2d.rotation = velocity.angle()
 	#velocity = velocity*(1-delta) + get_local_mouse_position()*delta*3
 	
 	#var p2 = get_local_mouse_position()/2 + velocity.project(get_local_mouse_position().orthogonal())
