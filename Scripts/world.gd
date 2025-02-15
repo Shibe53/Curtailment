5de09eb1c7329e01250d5ed3999a18aa5e2093a5
Şerban <r.serban@student.tue.nl>
2025-02-15 15:30:33 +0100

    Level functionality for world script

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+@export var level = 0
+
 var stats = PlayerStats
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().get_root().set_transparent_background(true)
-	stats.screens_needed = 1
+	match level:
+		0:
+			stats.screens_needed = 1
+		1:
+			pass
+		2: 
+			pass
+		3:
+			pass
+		4:
+			pass
 	stats.screens = 0
 	stats.all_screens_charged.connect(level_complete)
 
