860cb01388ec1f11f9f2434c8be420f9479b48a4
Şerban <r.serban@student.tue.nl>
2025-02-15 14:23:38 +0100

    Merge remote-tracking branch 'origin/windoverhaul2'

--- Scripts/no_cursor.gd
+++ Scripts/no_cursor.gd
@@ -1,25 +1,15 @@
 extends Area2D
 
 @export var shader_material: ShaderMaterial  # Assign this in the editor
+@onready var collision_shape_2d = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _process(delta):
-	var rat = get_node_or_null("../../Mouse/Sprite2D")
-	if shader_material:
-		var mask_pos = global_position
-		var sprite_pos = rat.global_position
-
-		# Convert to local space (relative to Sprite2D)
-		var local_mask_pos = (mask_pos - sprite_pos) / rat.scale  
-		var uv_mask_pos = local_mask_pos
-
-		# Get Area2D's dimensions (assuming CollisionShape2D with a rectangle)
-		var shape = $CollisionShape2D.shape
-		var mask_size = shape.extents * 2.0  # Full width & height
-		var uv_mask_size = mask_size
-
-		shader_material.set_shader_parameter("mask_position", uv_mask_pos)
-		shader_material.set_shader_parameter("mask_size", uv_mask_size)  # Pass size
+	pass
+	
+func _on_mouse_entered():
+	collision_shape_2d.shape
+	get_local_mouse_position()
