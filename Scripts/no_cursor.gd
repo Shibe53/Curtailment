1efe4b67c192901d9ef17ddf11c1576e432bb7c8
Lore <lorenyanski@gmail.com>
2025-02-15 11:52:15 +0100

    More areas

---
+++ Scripts/no_cursor.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var shader_material: ShaderMaterial  # Assign this in the editor
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _process(delta):
+	var rat = get_node_or_null("../../Mouse/Sprite2D")
+	if shader_material:
+		var mask_pos = global_position
+		var sprite_pos = rat.global_position
+
+		# Convert to local space (relative to Sprite2D)
+		var local_mask_pos = (mask_pos - sprite_pos) / rat.scale  
+		var uv_mask_pos = local_mask_pos
+
+		# Get Area2D's dimensions (assuming CollisionShape2D with a rectangle)
+		var shape = $CollisionShape2D.shape
+		var mask_size = shape.extents * 2.0  # Full width & height
+		var uv_mask_size = mask_size
+
+		shader_material.set_shader_parameter("mask_position", uv_mask_pos)
+		shader_material.set_shader_parameter("mask_size", uv_mask_size)  # Pass size
