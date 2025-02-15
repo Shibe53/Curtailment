08dfadd2990db78f78d88c54bc8da81f39f97d86
Şerban <r.serban@student.tue.nl>
2025-02-15 04:23:29 +0100

    Shader shenanigans

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -16,8 +16,8 @@ func _process(delta):
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	visible = true
+	pass
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	visible = false
+	pass
