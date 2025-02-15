2986402a81b2a479afd98e1d236c5e95fc012c19
Şerban <r.serban@student.tue.nl>
2025-02-15 17:20:22 +0100

    Level 2 and 4

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -3,16 +3,19 @@ extends Control
 var stats = PlayerStats
 
 func _on_button_pressed() -> void:
+	if stats.screens >= stats.screens_needed:
+		stats.level += 1
+	
 	match stats.level:
-		-1:
-			get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
 		0:
-			get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+			get_tree().change_scene_to_file("res://Scenes/Levels/tutorial_level.tscn")
 		1:
+			get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+		2:
 			get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
-		2: 
+		3: 
 			get_tree().change_scene_to_file("res://Scenes/Levels/level_3.tscn")
-		3:
-			get_tree().change_scene_to_file("res://Scenes/Levels/level_4.tscn")
 		4:
+			get_tree().change_scene_to_file("res://Scenes/Levels/level_4.tscn")
+		5:
 			pass
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -7,6 +7,9 @@ var screen2
 var screen3
 var screen4
 var screen5
+var screen6
+var screen7
+var screen8
 var stats = PlayerStats
 
 # Called when the node enters the scene tree for the first time.
@@ -19,7 +22,7 @@ func _ready() -> void:
 			stats.screens_needed = 1
 			stats.level = 1
 		2: 
-			stats.screens_needed = 8
+			stats.screens_needed = 4
 			stats.level = 2
 			screen2 = $WindowScreen2
 			screen3 = $WindowScreen3
@@ -30,7 +33,20 @@ func _ready() -> void:
 		3:
 			stats.level = 3
 		4:
+			stats.screens_needed = 8
 			stats.level = 4
+			screen3 = $WindowScreen3
+			screen4 = $WindowScreen4
+			screen5 = $WindowScreen5
+			screen6 = $WindowScreen6
+			screen7 = $WindowScreen7
+			screen8 = $WindowScreen8
+			screen3.deactivate()
+			screen4.deactivate()
+			screen5.deactivate()
+			screen6.deactivate()
+			screen7.deactivate()
+			screen8.deactivate()
 	stats.screens = 0
 	stats.all_screens_charged.connect(level_complete)
 
@@ -53,7 +69,21 @@ func _process(delta: float) -> void:
 		3:
 			pass
 		4:
-			pass
+			if stats.screens >= 2 and stats.screens < 4:
+				if screen3 != null:
+					screen3.activate()
+				if screen4 != null:
+					screen4.activate()
+			if stats.screens >= 4 and stats.screens < 6:
+				if screen5 != null:
+					screen5.activate()
+				if screen6 != null:
+					screen6.activate()
+			if stats.screens >= 6:
+				if screen7 != null:
+					screen7.activate()
+				if screen8 != null:
+					screen8.activate()
 
 func level_complete():
 	await get_tree().create_timer(3).timeout
