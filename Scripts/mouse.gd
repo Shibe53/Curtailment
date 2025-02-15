8eb51b8444456d089d8edce7286865a2625e5b5f
Şerban <r.serban@student.tue.nl>
2025-02-15 04:27:30 +0100

    Small fixes

--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -5,7 +5,6 @@ const FRICTION = 0.98
 
 @onready var sprite_2d = $Sprite2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = Vector2(0,0)
@@ -21,10 +20,3 @@ func _process(delta):
 	#var p2 = get_local_mouse_position()/2 + velocity.project(get_local_mouse_position().orthogonal())
 	#velocity = velocity*(1-delta) + p2*delta
 	#velocity += position.bezier_derivative(p2, p2, get_global_mouse_position(), 0)*delta
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	pass
